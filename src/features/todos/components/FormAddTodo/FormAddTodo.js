2781fdcc8bf88eb830b3efe8acb76a908d3c12ce
viet34tqc <viet34tqc@gmail.com>
2021-07-02 16:09:30 +0700

    Form Add to do

--- src/features/todos/components/FormAddTodo/FormAddTodo.js
+++ src/features/todos/components/FormAddTodo/FormAddTodo.js
@@ -1,11 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import'./FormAddTodo.styles.scss'
 const FormAddTodo = () => {
-    return (
-        <form>
-            
-        </form>
-    )
-}
+	return (
+		<form className="todo-form-add">
+			<input type="text" placeholder="Add your new todo" />
+		</form>
+	);
+};
 
-export default FormAddTodo
+export default FormAddTodo;
